0b409750950ee539fbfaf58b1927b6ee7f5f76d3
foreseaz <foreseazhang@gmail.com>
2019-03-25 02:16:43 +0800

    Example page

--- example/src/App.js
+++ example/src/App.js
@@ -5,8 +5,13 @@ import LoadCon from 'react-loadcon'
 export default class App extends Component {
   state = {
     status: 'normal',
-    type: 'donut',
+    type: 'pie',
     percentage: 0
+  }
+
+  handleTypeChange = (e) => {
+    this.setState({ type: e.target.value })
+    e.preventDefault()
   }
 
   asyncLoading = (hasError = false) => {
@@ -59,17 +64,58 @@ export default class App extends Component {
 
   render () {
     return (
-      <div>
+      <main className='container'>
         <LoadCon
           percentage={this.state.percentage}
           status={this.state.status}
           type={this.state.type}
         />
-
-        <button onClick={() => this.asyncLoading()}>start loading</button>
-        <button onClick={this.raiseException}>loading error</button>
-        <button onClick={this.raiseSuccess}>loading success</button>
-      </div>
+        <header className='nav'>
+          <span className='title'>React LoadCon</span>
+          <a
+            className='github-button'
+            href='https://github.com/foreseaz/react-loadcon'
+            data-icon='octicon-star'
+            data-size='large'
+            data-show-count='true'
+            aria-label='Star foreseaz/react-loadcon on GitHub'
+          >
+            Star
+          </a>
+        </header>
+        <div className='control'>
+          <div>
+            <label>Favicon loading type:</label>
+            <div className='select-wrapper'>
+              <select value={this.state.type} onChange={this.handleTypeChange}>>
+                <option value='pie'>Pie</option>
+                <option value='donut'>Donut</option>
+              </select>
+            </div>
+          </div>
+          <div className='btns'>
+            <button
+              onClick={() => this.asyncLoading()}
+            >
+              Start Loading
+            </button>
+            <button
+              onClick={this.raiseSuccess}
+              className='success'
+            >
+              Loading Success
+            </button>
+            <button
+              onClick={this.raiseException}
+              className='error'
+            >
+              Loading Error
+            </button>
+          </div>
+          <div className='star'>
+          </div>
+        </div>
+      </main>
     )
   }
 }
